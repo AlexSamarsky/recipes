7a07d37e3552ca78e21bf230235f71c543239b37
Samarskiy_A_S <samarskiy_a_s@MacBook-Pro-Sunlight.local>
2022-06-08 22:00:41 +0300

    2 items per page

--- recipes/views.py
+++ recipes/views.py
@@ -48,7 +48,7 @@ class RecipeFilter(FilterSet):
 
 
 class RecipeSetPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
