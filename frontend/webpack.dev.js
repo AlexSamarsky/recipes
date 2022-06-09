cd30839522ff54f999a6b02599c80987d4e5f6b1
Samarskiy_A_S <samarskiy_a_s@MacBook-Pro-Sunlight.local>
2022-06-09 21:32:35 +0300

    fix bug with plugins

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,21 +1,25 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const BundleTracker = require("webpack-bundle-tracker");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   //   context: path.resolve(__dirname, "src"),
   entry: "./src/index.js",
-  stats: "none",
+  // stats: "none",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       title: "Development",
       template: path.join(__dirname, "src", "index.html"),
       scriptLoading: "blocking",
     }),
+    new BundleTracker({ filename: "./webpack-stats.json" }),
   ],
   output: {
     filename: "main.js",
+    path: path.join(__dirname, "dist"),
   },
   mode: "development",
   devServer: {
@@ -42,5 +46,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
 };
